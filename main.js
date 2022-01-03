0f18c701c9cf6be2b123443d04918046afe06725
佳世 <96909506+HKayo@users.noreply.github.com>
2022-01-03 14:00:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-$(document).ready(function() {                                                           //When Document loads
-   $('.slide-section').click(function(e) {                           //When something that has slide section as class
-    var linkHref = $(this).attr('href');                          //Variable, storing what element slide section is pointing to
-    
-    $('html, body').animate({                               //Animates body scolling to
-     scrollTop: $(linkHref).offset().top + 1 //the object (linkHref) (+1 is optional, means it scrolls a pixel more)
-    }, 1000);                                                           //1000 means 1000ms which is 1 second
-    
-    e.preventDefault();                                        //Prevents href from showing in url
-   });
-});                                                                                                      //Hope you can understand the code
+on("click", function (event) {
+  var t = event.target,
+    tagName = t.tagName.toUpperCase(),
+    scrollPoint;
+  switch (tagName) {
+    case "IMG":
+    case "SVG":
+    case "USE":
+    case "U":
+    case "STRONG":
+    case "EM":
+    case "CODE":
+    case "S":
+    case "MARK":
+    case "SPAN":
+      while (!!(t = t.parentElement)) if (t.tagName == "A") break;
+      if (!t) return;
+      break;
+    default:
+      break;
+  }
+  if (t.tagName == "A" && t.getAttribute("href").substr(0, 1) == "#") {
+    if (
+      !!(scrollPoint = $(
+        '[data-scroll-id="' + t.hash.substr(1) + '"][data-scroll-invisible="1"]'
+      ))
+    ) {
+      event.preventDefault();
+      scrollToElement(scrollPoint);
+    } else if (t.hash == window.location.hash) {
+      event.preventDefault();
+      history.replaceState(undefined, undefined, "#");
+      location.replace(t.hash);
+    }
+  }
+});
