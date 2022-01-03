a673d927544068cf55d9d5fd14990be456a66b57
佳世 <96909506+HKayo@users.noreply.github.com>
2022-01-03 13:46:24 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-$(document).ready(fuction() {
-
-    var page_url = window.location.href;
-    var page_id = page_url.substring(page_url.lastIndexOf("#") + 1);
-        if (page_id == "honoo") {
-            $("html, body").animate({
-                scrollTop: $(page_id).offset().top
-            }, 1000);
-        }
-                  
-});
+$(document).ready(function() {                                                           //When Document loads
+   $('.slide-section').click(function(e) {                           //When something that has slide section as class
+    var linkHref = $(this).attr('href');                          //Variable, storing what element slide section is pointing to
+    
+    $('html, body').animate({                               //Animates body scolling to
+     scrollTop: $(linkHref).offset().top + 1 //the object (linkHref) (+1 is optional, means it scrolls a pixel more)
+    }, 1000);                                                           //1000 means 1000ms which is 1 second
+    
+    e.preventDefault();                                        //Prevents href from showing in url
+   });
+});                                                                                                      //Hope you can understand the code
